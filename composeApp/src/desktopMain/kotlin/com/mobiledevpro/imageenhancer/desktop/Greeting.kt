d5efc384765d1b2574e0d229de54eee254b9ca48
Dmitri Chernysh <dmitriy.chernysh@gmail.com>
2025-01-22 01:14:06 +0200

    Display a local image. Added Koin and ViewModel. Coil doesn't work by some reason

--- composeApp/src/desktopMain/kotlin/com/mobiledevpro/imageenhancer/desktop/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.mobiledevpro.imageenhancer.desktop
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
